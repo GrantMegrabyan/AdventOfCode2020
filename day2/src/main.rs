ca3d803af5a9ec087013c0fef6c6622ae012ebb7
Grant Megrabyan <grant.megrabyan@gmail.com>
2021-02-18 01:20:04 +0000

    Day 2: valid passwords, new policy

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -6,24 +6,46 @@ use shared::{MyError, read_first_arg};
 fn main() -> Result<(), MyError> {
     let input = read_first_arg()?;
 
-    let f = File::open(input)?;
+    println!("Valid passwords, policy 1: {}", policy1(&input)?);
+    println!("Valid passwords, policy 2: {}", policy2(&input)?);
+
+    Ok(())
+}
+
+fn policy1(file_path: &str) -> Result<i32, MyError> {
+    let f = File::open(file_path)?;
     let f = BufReader::new(f);
 
     let mut valid_cnt = 0;
-
     for line in f.lines() {
         let s = line.unwrap();
         let parts: Vec<&str> = s.split(' ').collect();
         let limits: Vec<usize> = parts[0].split('-').map(|n| n.parse().unwrap()).collect();
         let target = parts[1].chars().next().unwrap();
-        
         let cnt = parts[2].chars().filter(|ch| *ch == target).count();
         if limits[0] <= cnt && cnt <= limits[1] {
             valid_cnt += 1;
         }
     }
+    Ok(valid_cnt)
+}
 
-    println!("{}", valid_cnt);
+fn policy2(file_path: &str) -> Result<i32, MyError> {
+    let f = File::open(file_path)?;
+    let f = BufReader::new(f);
 
-    Ok(())
+    let mut valid_cnt = 0;
+    for line in f.lines() {
+        let s = line.unwrap();
+        let parts: Vec<&str> = s.split(' ').collect();
+        let pos: Vec<usize> = parts[0].split('-').map(|n| n.parse().unwrap()).collect();
+        let target = parts[1].chars().next().unwrap();
+        let chars: Vec<char> = parts[2].chars().collect();
+
+        match (chars[pos[0]-1] == target, chars[pos[1]-1] == target) {
+            (true, false) | (false, true) => valid_cnt += 1,
+            (_, _) => (),
+        }
+    }
+    Ok(valid_cnt)
 }
