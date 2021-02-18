b33f982d85e20b494b9411d9734d4a49179b3ed3
Grant Megrabyan <grant.megrabyan@gmail.com>
2021-02-18 23:04:51 +0000

    Day 3: Toboggan Trajectory, multiple slopes

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,8 +1,8 @@
+use shared::read_first_arg;
+use shared::MyError;
+use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
-use std::fs::File;
-use shared::MyError;
-use shared::read_first_arg;
 
 fn main() -> Result<(), MyError> {
     let input = read_first_arg()?;
@@ -10,18 +10,47 @@ fn main() -> Result<(), MyError> {
     let f = File::open(input)?;
     let f = BufReader::new(f);
 
-    let mut pos = 0;
-    let mut trees = 0;
+    let slopes = [
+        Slope { right: 1, down: 1 },
+        Slope { right: 3, down: 1 },
+        Slope { right: 5, down: 1 },
+        Slope { right: 7, down: 1 },
+        Slope { right: 1, down: 2 },
+    ];
+
+    let mut trees = vec![0; slopes.len()];
+    let mut pos = vec![0; slopes.len()];
+
+    let mut line_num = 0;
     for line in f.lines().skip(1) {
+        line_num += 1;
         let line = line.unwrap();
         let chars: Vec<char> = line.chars().collect();
-        pos = (pos + 3) % chars.len();
-        if chars[pos] == '#' {
-            trees += 1;
+
+        for i in 0..slopes.len() {
+            if line_num % slopes[i].down == 0 {
+                pos[i] = (pos[i] + slopes[i].right) % chars.len();
+                if chars[pos[i]] == '#' {
+                    trees[i] += 1;
+                }
+            }
         }
     }
 
-    println!("Trees: {}", trees);
-
+    println!("Trees: {:?}", trees);
+    println!(
+        "{} = {}",
+        trees
+            .iter()
+            .map(|n| n.to_string())
+            .collect::<Vec<String>>()
+            .join(" * "),
+        trees.iter().product::<u32>()
+    );
     Ok(())
+}
+
+struct Slope {
+    right: usize,
+    down: usize,
 }
