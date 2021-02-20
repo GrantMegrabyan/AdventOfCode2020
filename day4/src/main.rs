ae1aa7a08f5830fcb1b22b0168de78fc55dd44be
Grant Megrabyan <grant.megrabyan@gmail.com>
2021-02-20 00:54:50 +0000

    Day 4: Passport Processing with validation

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate lazy_static;
+use regex::Regex;
 use shared::read_first_arg;
 use shared::MyError;
 use std::collections::HashSet;
@@ -19,27 +22,77 @@ fn main() -> Result<(), MyError> {
 
     let mut fields_cnt = 0;
     let mut passport_cnt = 0;
+    let mut is_valid = true;
     for line in f.lines() {
         let line = line.unwrap();
         if line.is_empty() {
-            if fields_cnt == req_fields_cnt {
+            if fields_cnt == req_fields_cnt && is_valid {
                 passport_cnt += 1;
             }
+            is_valid = true;
             fields_cnt = 0;
         } else {
-            fields_cnt += line
-                .split(" ")
-                .filter_map(
-                    |p| match req_fields.contains(p.split(":").next().unwrap()) {
-                        true => Some(true),
-                        false => None,
-                    },
-                )
-                .count();
+            if !is_valid {
+                continue;
+            }
+            for (name, value) in line.split(' ').filter_map(|p| {
+                let field_parts: Vec<&str> = p.split(':').collect();
+                match req_fields.contains(field_parts[0]) {
+                    true => Some((field_parts[0], field_parts[1])),
+                    false => None,
+                }
+            }) {
+                fields_cnt += 1;
+                if !validate(name, value) {
+                    is_valid = false;
+                    break;
+                }
+            }
         }
     }
 
     println!("Passports: {}", passport_cnt);
 
     Ok(())
+}
+
+fn validate(name: &str, value: &str) -> bool {
+    lazy_static! {
+        static ref HCL_RE: Regex = Regex::new("^#[0-9a-f]{6}$").unwrap();
+        static ref HGT_RE: Regex = Regex::new(r"^(\d+)(cm|in)$").unwrap();
+        static ref PID_RE: Regex = Regex::new(r"^[\d]{9}$").unwrap();
+    }
+
+    match name {
+        "byr" => match value.parse::<i32>() {
+            Ok(val) => (1920..=2002).contains(&val),
+            Err(_) => false,
+        },
+        "iyr" => match value.parse::<i32>() {
+            Ok(val) => (2010..=2020).contains(&val),
+            Err(_) => false,
+        },
+        "eyr" => match value.parse::<i32>() {
+            Ok(val) => (2020..=2030).contains(&val),
+            Err(_) => false,
+        },
+        "ecl" => matches!(value, "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth"),
+        "hcl" => HCL_RE.is_match(value),
+        "hgt" => match HGT_RE.captures_iter(value).next() {
+            Some(cap) => match &cap[2] {
+                "cm" => match cap[1].parse::<i32>() {
+                    Ok(val) => (150..=193).contains(&val),
+                    Err(_) => false,
+                },
+                "in" => match cap[1].parse::<i32>() {
+                    Ok(val) => (59..=76).contains(&val),
+                    Err(_) => false,
+                },
+                _ => false,
+            },
+            None => false,
+        },
+        "pid" => PID_RE.is_match(value),
+        _ => false,
+    }
 }
