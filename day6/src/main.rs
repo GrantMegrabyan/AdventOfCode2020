850c481a5f494e4994f9e9723e1042de858ca14a
Grant Megrabyan <grant.megrabyan@gmail.com>
2021-02-22 00:45:24 +0000

    Day 6: Custom Customs, everyone

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,5 +1,5 @@
 use shared::{read_first_arg, MyError};
-use std::collections::HashSet;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
@@ -10,20 +10,24 @@ fn main() -> Result<(), MyError> {
     let f = File::open(input)?;
     let f = BufReader::new(f);
 
-    let mut group: HashSet<char> = HashSet::new();
+    let mut group: HashMap<char, usize> = HashMap::new();
+    let mut group_size: usize = 0;
     let mut result = 0;
     for line in f.lines() {
         let line = line.unwrap();
         if line.is_empty() {
-            result += group.len();
+            result += group.iter().filter(|(_, v)| **v == group_size).count();
             group.clear();
+            group_size = 0;
         } else {
+            group_size += 1;
             line.chars().into_iter().for_each(|ch| {
-                let _ = group.insert(ch);
+                let counter = group.entry(ch).or_insert(0);
+                *counter += 1;
             });
         }
     }
-    result += group.len();
+    result += group.iter().filter(|(_, v)| **v == group_size).count();
 
     println!("Sum: {}", result);
 
