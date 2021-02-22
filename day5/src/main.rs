2eef07a4eb94bf8ad4bbef70ea741f122c3b538f
Grant Megrabyan <grant.megrabyan@gmail.com>
2021-02-22 00:03:42 +0000

    Day 5: Binary Boarding, find my seat

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,10 +1,13 @@
 #[macro_use]
 extern crate lazy_static;
+use std::fmt;
+use std::cmp;
+use shared::{read_first_arg, MyError};
+use std::collections::BinaryHeap;
+use std::collections::HashMap;
+use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
-use std::fs::File;
-use shared::{read_first_arg, MyError};
-use std::collections::HashMap;
 
 fn main() -> Result<(), MyError> {
     let input = read_first_arg()?;
@@ -12,37 +15,83 @@ fn main() -> Result<(), MyError> {
     let f = File::open(input)?;
     let f = BufReader::new(f);
 
-    let mut max_seat_id = 0;
+    let mut heap = BinaryHeap::new();
     for line in f.lines() {
         let line = line.unwrap();
-        let seat_id = calc_seat_id(&line);
-        max_seat_id = std::cmp::max(max_seat_id, seat_id);
+        let seat = parse_seat(&line);
+        heap.push(seat);
     }
 
-    println!("Max seat id: {}", max_seat_id);
-    
+    let mut prev_seat = heap.pop().unwrap();
+    while let Some(seat) = heap.pop() {
+        if prev_seat.id - seat.id > 1 {
+            let my_seat_id = seat.id + 1;
+            println!("My seat id: {}", my_seat_id);
+            break;
+        }
+        prev_seat = seat;
+    }
+
     Ok(())
 }
 
-fn calc_seat_id(s: &str) -> u32 {
+fn parse_seat(s: &str) -> Seat {
     lazy_static! {
         static ref MAP: HashMap<char, u32> = [('F', 0), ('B', 1), ('L', 0), ('R', 1)]
             .iter()
             .cloned()
             .collect();
     }
-
     let mut row = 0;
     for ch in s[0..7].chars() {
         let bit = MAP.get(&ch).unwrap();
         row = (row << 1) | bit;
     }
-    
     let mut col = 0;
-    for ch in s[0..3].chars() {
+    for ch in s[7..].chars() {
         let bit = MAP.get(&ch).unwrap();
         col = (col << 1) | bit;
     }
+    Seat::new(row, col)
+}
 
-    row * 8 + col
+#[derive(Eq)]
+struct Seat {
+    row: u32,
+    col: u32,
+    id: u32,
+}
+
+impl Seat {
+    pub fn new(row: u32, col: u32) -> Self {
+        Seat {
+            row,
+            col,
+            id: row * 8 + col,
+        }
+    }
+}
+
+impl std::fmt::Display for Seat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "row {}, col {}, id {}", self.row, self.col, self.id)
+    }
+}
+
+impl cmp::Ord for Seat {
+    fn cmp(&self, other: &Self) -> cmp::Ordering {
+        self.id.cmp(&other.id)
+    }
+}
+
+impl cmp::PartialOrd for Seat {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Seat {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
 }
