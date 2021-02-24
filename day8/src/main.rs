bb8344710dda68e27e0c9a8277a844e7fe06ef3d
Grant Megrabyan <grant.megrabyan@gmail.com>
2021-02-24 23:26:55 +0000

    Day 8: Handheld Halting, fix the program

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -34,19 +34,54 @@ fn main() -> Result<(), MyError> {
     let mut accumulator = 0;
     let mut index: usize = 0;
     let mut visited: HashSet<usize> = HashSet::new();
-    while !visited.contains(&index) {
+    let mut stack: Vec<(usize, &str)> = Vec::new();
+    let mut is_changed = false;
+    loop {
+        let (mut op, arg) = instructions[index];
+
+        // If current op was already visited,
+        // start backtracking. Pop the last operation from the stack,
+        // if it's "acc" - subtract the value from accumulator,
+        // if it's "nop" or "jmp" - look at the original instructions list.
+        // If the same op is there, it means we haven't tried changing it
+        // to the opposite one - do it. Otherwise skip and go to the next
+        // element of the stack.
+        if visited.contains(&index) {
+            while let Some((ind, prev_op)) = stack.pop() {
+                visited.remove(&ind);
+                if prev_op == "acc" {
+                    accumulator -= instructions[ind].1;
+                } else if instructions[ind].0 == prev_op {
+                    if !is_changed {
+                        index = ind;
+                        op = if prev_op == "nop" { "jmp" } else { "nop" };
+                        is_changed = true;
+                        break;
+                    }
+                } else {
+                    is_changed = false;
+                }
+            }
+        }
+
         visited.insert(index);
-        match &instructions[index] {
-            ("nop", _) => index += 1,
-            ("acc", arg) => {
+        stack.push((index, op));
+        match op {
+            "nop" => index += 1,
+            "acc" => {
                 accumulator += arg;
                 index += 1;
             }
-            ("jmp", arg) => index = (index as i32 + arg) as usize,
-            (op, _) => panic!("Unsupported operation: {}", op),
+            "jmp" => index = (index as i32 + arg) as usize,
+            _ => panic!("Unsupported operation: {}", op),
+        }
+
+        if index >= instructions.len() {
+            break;
         }
     }
 
+    // println!("Stack: {:?}", stack);
     println!("Accumulator: {}", accumulator);
 
     Ok(())
