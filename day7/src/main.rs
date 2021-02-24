ea3864803bb9035c25f2dd5a820cb9596fd465a5
Grant Megrabyan <grant.megrabyan@gmail.com>
2021-02-24 00:25:12 +0000

    Day 7: Handy Haversacks, count inner bags

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,7 +1,6 @@
 use regex::Regex;
 use shared::{read_first_arg, MyError};
 use std::collections::HashMap;
-use std::collections::HashSet;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
@@ -12,44 +11,40 @@ fn main() -> Result<(), MyError> {
     let f = File::open(input)?;
     let f = BufReader::new(f);
 
-    let bag_re: Regex = Regex::new(r"(\w+ \w+) bag").unwrap();
-    let mut map: HashMap<String, HashSet<String>> = HashMap::new();
+    let outer_bag_re: Regex = Regex::new(r"(\w+ \w+) bag").unwrap();
+    let inner_bag_re: Regex = Regex::new(r"(\d+) (\w+ \w+) bag").unwrap();
+    let mut bags: HashMap<String, HashMap<String, usize>> = HashMap::new();
 
     for line in f.lines() {
         let line = line.unwrap();
         let parts: Vec<&str> = line.split(" contain ").collect();
 
-        let outer = match bag_re.captures_iter(parts[0]).next() {
+        let outer = match outer_bag_re.captures_iter(parts[0]).next() {
             Some(cap) => String::from(&cap[1]),
             None => panic!("Failed to parse outer bag"),
         };
 
-        let inner_bags: Vec<String> = bag_re
+        let inner_bags: HashMap<String, usize> = inner_bag_re
             .captures_iter(parts[1])
-            .map(|cap| String::from(&cap[1]))
+            .map(|cap| (String::from(&cap[2]), cap[1].parse::<usize>().unwrap()))
             .collect();
 
-        for inner in inner_bags {
-            let entry = map.entry(inner).or_insert_with(HashSet::new);
-            entry.insert(outer.clone());
-        }
+        bags.insert(outer, inner_bags);
     }
 
-    let mut visited: HashSet<&str> = HashSet::new();
-    let mut queue = vec!["shiny gold"];
-    while let Some(current) = queue.pop() {
-        if let Some(outer_bags) = map.get(current) {
-            for outer_bag in outer_bags {
-                if visited.contains(&(*outer_bag)[..]) {
-                    continue;
-                }
-                queue.push(outer_bag);
-                visited.insert(outer_bag);
-            }
-        }
-    }
-
-    println!("Answer: {}", visited.len());
+    println!("Answer: {}", get_inner_count(&bags, "shiny gold"));
 
     Ok(())
+}
+
+fn get_inner_count(bags: &HashMap<String, HashMap<String, usize>>, outer_bag: &str) -> usize {
+    let mut result = 0;
+
+    if let Some(inner_bags) = bags.get(outer_bag) {
+        for (inner_bag, count) in inner_bags {
+            result += count + count * get_inner_count(bags, inner_bag);
+        }
+    }
+
+    result
 }
